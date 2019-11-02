b1835373174a1b2f40f39886cd9a4ece210f2b6f
Gabriel Lima <gabriellfsouza@gmail.com>
2019-11-02 16:50:58 -0300

    chore: first commit

--- resource/views/utils.js
+++ resource/views/utils.js
@@ -1,20 +1,7 @@
-let online
-
-const urlParams = new URLSearchParams(window.location.search)
-
-const token = urlParams.get('token')
-const userId = urlParams.get('id')
-
-if (!token || !userId) {
-  alert('Você precisa passar o token e o id do usuário!')
-}
+let online = false
 
 function start () {
-  let ws = adonis.Ws('ws://localhost:3333')
-
-  ws = ws.withJwtToken(token)
-
-  ws = ws.connect()
+  const ws = adonis.Ws('ws://localhost:3333').connect()
 
   ws.on('open', () => {
     online = true
@@ -23,25 +10,4 @@ function start () {
   ws.on('error', () => {
     online = false
   })
-
-  return ws
-}
-
-function ajax (url, data, method = 'post') {
-  return new Promise((resolve, reject) => {
-    $.ajax(`http://localhost:3333${url}`, {
-      method,
-      headers: {
-        Authorization: `Bearer ${token}`
-      },
-      dataType: 'json',
-      data
-    })
-      .done(data => resolve(data))
-      .fail(e => reject(e))
-  })
-}
-
-function $likes (likes, type = 'post') {
-  $(`span[data-${type}-id="${likes.id}"]`).text(likes.count)
 }
